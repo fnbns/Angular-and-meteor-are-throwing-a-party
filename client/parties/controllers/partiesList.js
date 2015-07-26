5eb2f16f7e30c43cb8d05f17824aa1b5c5e3b884
Alex Serban <serban.alexc@gmail.com>
2015-07-27 00:05:15 +0300

    Implemented CUD restriction for Parties Collection

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -1,7 +1,15 @@
- angular.module('campusParty').controller('PartiesListCtrl', ['$scope', '$meteor',
-        function ($scope, $meteor) {
+ angular.module('campusParty').controller('PartiesListCtrl', ['$scope', '$meteor', '$root',
+        function ($scope, $meteor, $root) {
 
          $scope.parties = $meteor.collection(Parties)
+
+
+         /* add party method*/
+         $scope.addParty = function () {
+             $scope.newParty.owner = $root.currentUser._id
+             $scope.parties.push($scope.newParty)
+         }
+
 
          $scope.remove = function (party) {
              $scope.parties.splice($scope.parties.indexOf(party), 1)
