2ae48898084629545f096024df534d77481e60c2
Alex Serban <serban.alexc@gmail.com>
2015-07-27 00:35:20 +0300

    Implemented CUD Restrictions for Parties & Configured Twitter Auth.

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -1,22 +1,30 @@
- angular.module('campusParty').controller('PartiesListCtrl', ['$scope', '$meteor', '$root',
-        function ($scope, $meteor, $root) {
+ angular.module('campusParty')
+     .controller('PartiesListCtrl', ['$scope', '$meteor', '$rootScope',
+        function ($scope, $meteor, $rootScope) {
 
-         $scope.parties = $meteor.collection(Parties)
+             $scope.parties = $meteor.collection(Parties)
 
+             /* add party*/
+             $scope.addParty = function () {
+                 if ($rootScope.currentUser) {
+                     $scope.newParty.owner = $rootScope.currentUser._id
+                     $scope.parties.push($scope.newParty)
 
-         /* add party method*/
-         $scope.addParty = function () {
-             $scope.newParty.owner = $root.currentUser._id
-             $scope.parties.push($scope.newParty)
-         }
+                     /* reset model*/
+                     $scope.newParty = {}
 
+                 } else {
+                     /* TODO: Show allert / notify user */
+                 }
 
-         $scope.remove = function (party) {
-             $scope.parties.splice($scope.parties.indexOf(party), 1)
-         }
+             }
 
-         $scope.removeAll = function () {
-             $scope.parties.remove()
-         };
+             $scope.remove = function (party) {
+                 $scope.parties.splice($scope.parties.indexOf(party), 1)
+             }
+
+             $scope.removeAll = function () {
+                 $scope.parties.remove()
+             };
 
       }])
--- client/routes.js
+++ client/routes.js
@@ -1,19 +1,36 @@
- angular.module('campusParty').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+angular.module("campusParty").run(["$rootScope", "$state", function ($rootScope, $state) {
+    $rootScope.$on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
+
+        /* catching the error from $meteor.requireUser(); get back to home*/
+        if (error === "AUTH_REQUIRED") {
+            console.log("error on auth, return to parties")
+            $state.go('parties')
+        }
+    })
+}])
+
+angular.module('campusParty').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
   function ($urlRouterProvider, $stateProvider, $locationProvider) {
 
-         $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
 
-         $stateProvider
-             .state('parties', {
-                 url: '/parties',
-                 templateUrl: 'client/parties/views/parties-list.ng.html',
-                 controller: 'PartiesListCtrl'
-             })
-             .state('partyDetails', {
-                 url: '/parties/:partyId',
-                 templateUrl: 'client/parties/views/party-details.ng.html',
-                 controller: 'PartyDetailsCtrl'
-             });
+        $stateProvider
+            .state('parties', {
+                url: '/parties',
+                templateUrl: 'client/parties/views/parties-list.ng.html',
+                controller: 'PartiesListCtrl'
+            })
+            .state('partyDetails', {
+                url: '/parties/:partyId',
+                templateUrl: 'client/parties/views/party-details.ng.html',
+                controller: 'PartyDetailsCtrl',
+                resolve: {
+                    "currentUser": ["$meteor", function ($meteor) {
+                        return $meteor.requireUser();
+    }]
+                }
+            })
+            //TODO #1: Implement require valid users for priviledge levels !
 
-         $urlRouterProvider.otherwise('/parties')
+        $urlRouterProvider.otherwise('/parties')
 }])
